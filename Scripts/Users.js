1ee18c640b68ca1f833e100febfe619fc6cff9e6
Justin Xie <justinjayxie@gmail.com>
2019-11-27 01:45:57 -0800

    updated the navbar

--- Scripts/Users.js
+++ Scripts/Users.js
@@ -8,13 +8,39 @@ function createUser() {
     // is authenticated, then grab "uid" "displayName" and "email"
     // use "set()" with merge (if document did not exist it will be created)
     firebase.auth().onAuthStateChanged(function(user){
-        
+        if (user) {
             db.collection("user").doc(user.uid).set(
                 {
                 "name":user.displayName,
                  "email":user.email,
                 },{ merge: true });
- 
+            } else {
+                console.log("no one signed in");
+            }
+
     });
 }
 
+   
+function showName() {
+    
+    firebase.auth().onAuthStateChanged(function (user) {
+      console.log(user);
+      document.getElementById("hello").innerHTML = user.displayName;
+    });
+  
+
+  }
+
+
+
+  
+(function () {
+    var but = document.getElementById("logMeOut");
+    but.onclick = firebase.auth().signOut();
+    but.addEventListener('click', e=> {
+        firebase.auth().signOut();
+        console.log("logged out");
+        location.reload();
+    });
+}());
