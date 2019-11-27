a3b9a9385d45a100d1745145d1d921323f7cb378
Abeer Haroon <abr.haroon@gmail.com>
2019-11-26 20:30:58 -0800

    createAGroupTrialAbr.js is now script file of create_a_group.html. Create a Group form now writes to firestore: groupName, details, location and course

--- Scripts/createAGroupTrialAbr.js
+++ Scripts/createAGroupTrialAbr.js
@@ -5,34 +5,23 @@ generate time stamp to put in "Time Created" */
 
 
 $(document).ready(function() {
+  
     
     /*Date object to add to a data field in the future*/
     var currentDateandTime = new Date();
    
-    $(document).on('click', 'button#create_group_btn', function() {
-    
-    /*each variable is scanning the user input */
-    var nameOfGroup = document.getElementById("groupNameID").value;
-
-    var nameOfCourse = document.getElementById("courseSelectID").value;
-
-    var setLocation = document.getElementById("groupLocationID").value;
-
-    var detailsOfGroup = document.getElementById("groupDetailsID").value;
-
-    //adding a document with the following data in collection "Groups"
-        db.collection("Groups").add({
-            groupName: nameOfGroup,
-            course: nameOfCourse,
-            location: setLocation,
-            details: detailsOfGroup
-        })
-        .then(function() {
-            console.log("Document successfully written!");
-        })
-        .catch(function(error) {
-            console.error("Error writing document: ", error);
-        })
+    var groupForm =  document.querySelector('#createAGroupForm');
+  
+    groupForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      
+      db.collection('Groups').add({
+        groupName: groupForm.groupNameA.value,
+        course: groupForm.courseSelectA.value,
+        location: groupForm.groupLocationA.value,
+        details: groupForm.groupDetailsA.value,
+        
+      })
     });
 });
 
